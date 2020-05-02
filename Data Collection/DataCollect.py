82a3e4d7e9ab6428c45eedd3ecef31c31b2b9f0b
MedChebbi <1mohamed.chebbi@gmail.com>
2020-05-02 23:06:32 +0100

    Data collection update

--- Data Collection/DataCollect.py
+++ Data Collection/DataCollect.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
-import serial
+import serial #Serial communication library
 from time import sleep
 import csv
 
-ser= serial.Serial('/dev/ttyACM0',2400,timeout=0.5)
+ser= serial.Serial('/dev/ttyACM0',2400,timeout=0.5) #Serial communication between the arduino and the PC
 ser.flushInput()
 d=0
-i=0
+i=0 #i is the counter for the data added to the dataset
 
 def make_resolution():
     #cap.set(3, 352)
@@ -17,31 +17,26 @@ def make_resolution():
     #cap.set(3, 640)
     #cap.set(4, 480)
 
-
 def rescale_frame(frame, percent=75):
     width = int(rows * percent/ 100)
     height = int(cols * percent/ 100)
     dim = (width, height)
     return cv2.resize(frame, dim)
 
-#print(ser.name)
-
-face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml') #load the face detection haarcascade model
 
 cap= cv2.VideoCapture(0)
-make_resolution()
+make_resolution() #fix the resolution you want
 _, frame = cap.read()
 
 rows,cols,_=frame.shape
-#print(rows,cols)
+
 sleep(2)
-#with open('data_set.csv','w',newline='')as f:
-with open('data_set.csv','a',newline='')as f:
+#with open('data_set.csv','w',newline='')as f: #This is when you want a new data_set to make
+with open('data_set.csv','a',newline='')as f: #Adding data to the dataset data_set.csv
     thewriter=csv.writer(f)
-    #thewriter.writerow(['x_resol','y_resol','w_values','h_values','target'])
-    while (i<80):
-        #ser_bytes = ser.readline()
-        #d=ser_bytes.decode().rstrip()
+    #thewriter.writerow(['x_resol','y_resol','w_values','h_values','target']) #this line is for making a new dataset
+    while (i<300):
         print(d)
         #timer = cv2.getTickCount()
         ret, frame = cap.read()
@@ -52,10 +47,9 @@ with open('data_set.csv','a',newline='')as f:
 
         for (x, y, w, h) in faces:
             #print(x,y,w,h)
-            #d=ser_bytes
-            ser_bytes = ser.readline()
-            d=ser_bytes.decode().rstrip()
-            thewriter.writerow([cols,rows,w,h,d])
+            ser_bytes = ser.readline() #read data from distance sensor in Arduino
+            d=ser_bytes.decode().rstrip() #convert data
+            thewriter.writerow([cols,rows,w,h,d]) #add data to the dataset
             i+=1
             roi_gray = gray[y:y+h, x:x+w]
             roi_color = frame[y:y, x:x+w]
